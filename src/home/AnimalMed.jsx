b1596bc0da9e75125fa92d2033dbb359aaa060b9
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-03-22 22:35:48 -0300

    add: components,services,home,styles

---
+++ src/home/AnimalMed.jsx
@@ -0,0 +1,12 @@
+import "../styles/App.css";
+
+function AnimalMed() {
+  return (
+    <div>
+      <h1>hello word</h1>
+    </div>
+  );
+}
+
+export default AnimalMed;
+ 
---
+++ src/services/AnimalMedApi.js
@@ -0,0 +1,3 @@
+const urlApi =
+  "https://api-animal-dot-api-samples-423102.uc.r.appspot.com/api/animais";
+console.log("urlApi", urlApi);
