7425049359b299d519f918684d5edd15f51c6ef0
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-03-24 23:05:24 -0300

    refactor: Get consumindo api do swagger

--- src/home/AnimalMed.jsx
+++ src/home/AnimalMed.jsx
@@ -1,12 +1,50 @@
 import "../styles/App.css";
+ import { useState } from "react";
+import { fidAll } from "../services/AnimalMedApi";
 
 function AnimalMed() {
+  const [animais, setAnimais] = useState([]);
+
+  const pesquisar = async () => {
+    console.log("Consultando os animais cadastrados");
+    const dados = await fidAll();
+    setAnimais(dados);
+  }; 
+
   return (
-    <div>
-      <h1>hello word</h1>
+    <div className="Conteiner">
+      <div className="conteudo">
+        <div className="titulo-container">
+          <h1 className="titulo">Cadastro de Animais</h1>
+        </div>
+
+        <div className="formulario-container">
+          <button className="button" onClick={pesquisar}>Consultar</button>
+        </div>
+
+        <div className="cards-container">
+          <div className="cards">
+            {animais.map((a, index) => (
+              <div key={index} className="card">
+                <p>
+                  <strong>Nome:</strong> <span>{a.nome}</span>
+                </p>
+                <p>
+                  <strong>Data de Nascimento:</strong> <span>{a.dataNascimento}</span>
+                </p>
+                <p>
+                  <strong>Sexo:</strong> <span>{a.sexo}</span>
+                </p>
+                <p>
+                  <strong>Espécie:</strong> <span>{a.especie}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default AnimalMed;
- 
--- src/services/AnimalMedApi.js
+++ src/services/AnimalMedApi.js
@@ -1,3 +1,22 @@
 const urlApi =
   "https://api-animal-dot-api-samples-423102.uc.r.appspot.com/api/animais";
-console.log("urlApi", urlApi);
+
+export async function fidAll() {
+  const requestInfo = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer 12119065",
+    },
+  };
+
+  const responseHttp = await fetch(urlApi, requestInfo);
+
+  if (responseHttp.ok) {
+    return await responseHttp.json();
+  } else {
+    console.log("falha de consulta dados erro 400");
+    throw new Error("falha de consulta dados erro 400");
+  }
+}
+
