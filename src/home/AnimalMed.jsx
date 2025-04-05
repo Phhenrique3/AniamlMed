2ee3d532ca3b92497ef3e6a068ec1a41c17f6ec7
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-04-04 23:36:05 -0300

    Post

--- src/home/AnimalMed.jsx
+++ src/home/AnimalMed.jsx
@@ -1,15 +1,46 @@
 import "../styles/App.css";
- import { useState } from "react";
-import { findAll } from "../services/AnimalMedApi";
+import { useState, useRef } from "react";
+import { findAll, insert } from "../services/AnimalMedApi";
 
 function AnimalMed() {
   const [animais, setAnimais] = useState([]);
 
+  const inputNome = useRef();
+  const inputDataNascimento = useRef();
+  const inputSexo = useRef();
+  const inputEspecie = useRef();
+
+  const salvar = async () => {
+    const nome = inputNome.current.value;
+    const dataNascimento = inputDataNascimento.current.value;
+    const sexo = inputSexo.current.value;
+    const especie = parseInt(inputEspecie.current.value);
+
+    console.log("salvando o cadastro do animal", nome, dataNascimento, sexo, especie);
+
+    if (isNaN(especie)) {
+      alert("Campo espécie deve ser um número");
+      return;
+    }
+
+    await insert(nome, dataNascimento, sexo, especie);
+
+    console.log("animal salvo com sucesso");
+
+    // Limpa os campos
+    inputNome.current.value = "";
+    inputDataNascimento.current.value = "";
+    inputSexo.current.value = "";
+    inputEspecie.current.value = "";
+
+    await pesquisar();
+  };
+
   const pesquisar = async () => {
-    console.log("Consultando os animais cadastrados");
+    console.log("consultando os animais cadastrados");
     const dados = await findAll();
     setAnimais(dados);
-  }; 
+  };
 
   return (
     <div className="Conteiner">
@@ -17,11 +48,39 @@ function AnimalMed() {
         <div className="titulo-container">
           <h1 className="titulo">Cadastro de Animais</h1>
         </div>
-
-        <div className="formulario-container">
-          <button className="button" onClick={pesquisar}>Consultar</button>
-        </div>
-
+        <input
+          placeholder="Nome"
+          name="nome"
+          id="nome"
+          type="text"
+          ref={inputNome}
+        />
+        Data Nascimento
+        <input
+          placeholder="yyyy/mm/dd"
+          name="dataNascimento"
+          id="dataNascimento"
+          type="text"
+          ref={inputDataNascimento}
+        />
+        Sexo
+        <input
+          placeholder="M ou F"
+          name="sexo"
+          id="sexo"
+          type="text"
+          ref={inputSexo}
+        />
+        Espécie
+        <input
+          placeholder="Código da espécie (número)"
+          name="especie"
+          id="especie"
+          type="number"
+          ref={inputEspecie}
+        />
+        <button onClick={salvar}>Salvar</button>
+        <button onClick={pesquisar}>Consultar</button>
         <div className="cards-container">
           <div className="cards">
             {animais.map((a, index) => (
@@ -30,7 +89,8 @@ function AnimalMed() {
                   <strong>Nome:</strong> <span>{a.nome}</span>
                 </p>
                 <p>
-                  <strong>Data de Nascimento:</strong> <span>{a.dataNascimento}</span>
+                  <strong>Data de Nascimento:</strong>{" "}
+                  <span>{a.dataNascimento}</span>
                 </p>
                 <p>
                   <strong>Sexo:</strong> <span>{a.sexo}</span>
--- src/services/AnimalMedApi.js
+++ src/services/AnimalMedApi.js
@@ -19,3 +19,26 @@ export async function findAll() {
     throw new Error("falha de consulta dados erro 400");
   }
 }
+
+export async function insert(nome, dataNascimento, sexo, especie) {
+
+  console.log(insert)
+  const requestInfo = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json", // Adiciona isso aqui!
+      Authorization: "Bearer " + apiToken,
+    },
+    body:JSON.stringify({ nome, dataNascimento, sexo, especie }),
+  };
+
+  const responseHttp = await fetch(urlApi, requestInfo);
+
+  if (responseHttp.ok) {
+    return await responseHttp.json();
+  } else {
+    console.log("falha de consulta dados erro 400");
+    throw new Error("falha de consulta dados erro 400");
+  }
+}
